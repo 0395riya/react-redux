8257a53ef9fea80a71ceddfb55863a99f512f376
Archana Kumari <archana@valueinnovationlabs.com>
2023-06-08 16:46:46 +0530

    redux_react

---
+++ src/BookList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const BookList = () => {
+  return (
+    <React.Fragment>
+    <div className='container'>
+<h1>Book On</h1>
+<div className='book_list'>
+<input>
+
+</input>
+<button>Add Book</button>
+</div>
+    </div>
+
+      
+    </React.Fragment>
+  )
+}
+
+export default BookList
