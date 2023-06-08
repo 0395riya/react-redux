89a1eaa696d4ca2427a982069f32f357ea04b295
Archana Kumari <archana@valueinnovationlabs.com>
2023-06-08 17:07:37 +0530

    reduxapp

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <>
       <div className='container'>
         <h1>Increment/Decrement counter</h1>
-        <h4>using React and Redux</h4>
+        <h4>Using React and Redux</h4>
         <div className='quantity'>
           <a className='quantity_minus' title='Decrement'  onClick={() => dispatch(decNumber())}><span>-</span></a>
           <input name='quantity' type='text' className='quantity_input' value={myState}></input>
